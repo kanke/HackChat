e33dd67381ea5bbc198029da73fa23b7225173d6
unknown <estelletidey@VOCLTP004.vocality.local>
2015-03-07 16:13:17 +0000

    Adding project

---
+++ Application/src/main/java/com/example/android/bluetoothchat/ChatMessage.java
@@ -0,0 +1,15 @@
+package com.example.android.bluetoothchat;
+
+/**
+ * Created by estelletidey on 07/03/2015.
+ */
+public class ChatMessage {
+    public String user;
+    public String msg;
+
+    public ChatMessage(String user, String msg)
+    {
+        this.user = user;
+        this.msg = msg;
+    }
+}
---
+++ Application/src/main/java/com/example/android/bluetoothchat/ChatMessageAdapter.java
@@ -0,0 +1,38 @@
+package com.example.android.bluetoothchat;
+
+import android.widget.ArrayAdapter;
+import android.content.Context;
+import com.example.android.bluetoothchat.ChatMessage;
+import java.util.ArrayList;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.LayoutInflater;
+import android.widget.TextView;
+
+/**
+ * Created by estelletidey on 07/03/2015.
+ */
+public class ChatMessageAdapter extends ArrayAdapter<ChatMessage> {
+
+    public ChatMessageAdapter(Context context, ArrayList<ChatMessage> messages) {
+        super(context, 0, messages);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Get the data item for this position
+        ChatMessage chatMessage = getItem(position);
+        // Check if an existing view is being reused, otherwise inflate the view
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.message, parent, false);
+        }
+        // Lookup view for data population
+        TextView msgUser = (TextView) convertView.findViewById(R.id.message_user);
+        TextView msgText = (TextView) convertView.findViewById(R.id.message_msg);
+        // Populate the data into the template view using the data object
+        msgUser.setText(chatMessage.user);
+        msgText.setText(chatMessage.msg);
+        // Return the completed view to render on screen
+        return convertView;
+    }
+}

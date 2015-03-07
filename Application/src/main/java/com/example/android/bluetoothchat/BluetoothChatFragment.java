e04d271e6f3a42e30f0fbc395893e14d72cdc05f
kanke Ishaku <kanke.ishaku@live.com>
2015-03-07 17:37:41 +0000

    Preserve message history
    commented out to not clear chat history after device has been connected

--- Application/src/main/java/com/example/android/bluetoothchat/BluetoothChatFragment.java
+++ Application/src/main/java/com/example/android/bluetoothchat/BluetoothChatFragment.java
@@ -292,7 +292,7 @@ public class BluetoothChatFragment extends Fragment {
                     switch (msg.arg1) {
                         case BluetoothChatService.STATE_CONNECTED:
                             setStatus(getString(R.string.title_connected_to, mConnectedDeviceName));
-                            mConversationArrayAdapter.clear();
+                          //  mConversationArrayAdapter.clear();
                             break;
                         case BluetoothChatService.STATE_CONNECTING:
                             setStatus(R.string.title_connecting);
